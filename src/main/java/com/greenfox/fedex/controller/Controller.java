282128615733215de9e7236a9e1a2773c6efb61d
zsiguli <markovichzsigmond@gmail.com>
2017-07-06 19:46:13 +0200

    fix(compatibility): restore endpoints

--- src/main/java/com/greenfox/fedex/controller/Controller.java
+++ src/main/java/com/greenfox/fedex/controller/Controller.java
@@ -18,8 +18,44 @@ public class Controller {
   private UserService userService;
 
   @Autowired
-  public Controller(UserService userService) {
+  public Controller (UserService userService) {
     this.userService = userService;
+  }
+
+  @GetMapping("/api/best_times")
+  public Data bestTimes(HttpServletRequest request) {
+    Links links = new Links(request.getRequestURL().toString());
+    links.setNext("next");
+    links.setPrev("prev");
+    links.setLast("last");
+    return new Data(userService.findBestTimes(), links);
+  }
+
+  @GetMapping("/api/best_no_spins")
+  public Data bestNumberOfSpins(HttpServletRequest request) {
+    Links links = new Links(request.getRequestURL().toString());
+    links.setNext("next");
+    links.setPrev("prev");
+    links.setLast("last");
+    return new Data(userService.findBestNumberOfSpins(), links);
+  }
+
+  @GetMapping("/api/best_avg_rpms")
+  public Data bestAvgRpms(HttpServletRequest request) {
+    Links links = new Links(request.getRequestURL().toString());
+    links.setNext("next");
+    links.setPrev("prev");
+    links.setLast("last");
+    return new Data(userService.findBestAvgRpms(), links);
+  }
+
+  @GetMapping("/api/best_max_rpms")
+  public Data bestMaxRpms(HttpServletRequest request) {
+    Links links = new Links(request.getRequestURL().toString());
+    links.setNext("next");
+    links.setPrev("prev");
+    links.setLast("last");
+    return new Data(userService.findBestMaxRpms(), links);
   }
 
   @GetMapping("/user/{nickName}")

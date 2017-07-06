2a77e05fb646da22f06860029aa941d6637c61e0
zsiguli <markovichzsigmond@gmail.com>
2017-07-06 11:19:08 +0200

    chore(mainController): create endpoint to test

--- src/main/java/com/greenfox/fedex/controller/Controller.java
+++ src/main/java/com/greenfox/fedex/controller/Controller.java
@@ -1,16 +1,24 @@
 package com.greenfox.fedex.controller;
 
 import com.greenfox.fedex.model.User;
+import com.greenfox.fedex.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
 
+  private UserService userService;
+
+  @Autowired
+  public Controller (UserService userService) {
+    this.userService = userService;
+  }
+  
   @GetMapping("/users")
   public Page<User> returnAllTheUsers() {
-    return userService.findAll();
+    return userService.findAllTheUsers();
   }
 }
